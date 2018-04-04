e17f418727c206bcbfa13bbb41d7feb185eeb372
wangs PC <wonsaign@yeah.net>
2018-04-04 15:32:02 +0800

    commit maven project

---
+++ src/main/java/com/learn/java/lang/wangs/App.java
@@ -0,0 +1,13 @@
+package com.learn.java.lang.wangs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
