33eb0a1ce4d8948385b71d3e475cb25993ae1921
wangs <1031897661@qq.com>
2018-04-09 21:08:47 +0800

    inner class

---
+++ src/main/java/com/learn/java/lang/wangs/test/InnerClass.java
@@ -0,0 +1,14 @@
+package com.learn.java.lang.wangs.test;
+
+public class InnerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	class in{
+		
+	}
+}
