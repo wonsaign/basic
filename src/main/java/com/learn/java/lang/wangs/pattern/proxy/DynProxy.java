acd775212e624d414a33016b31b5360cd99dce60
wangs <wangsai@jdab119.com>
2018-07-28 14:59:55 +0800

    add util

--- src/main/java/com/learn/java/lang/wangs/pattern/proxy/DynProxy.java
+++ src/main/java/com/learn/java/lang/wangs/pattern/proxy/DynProxy.java
@@ -5,18 +5,25 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class DynProxy implements InvocationHandler{
+	
+	private Object obj;
+	
+	public DynProxy(Object obj) {
+		this.obj = obj;
+	}
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		System.out.println("代理开始");
 		System.out.println("我自己加了方法");
+		method.invoke(obj, args);
 		System.out.println("代理结束");
 		return null;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T> T newMapperProxy(Class<T> t) {
+	public static  <T> T newMapperProxy(Class<? extends T> t) {
 		return (T)Proxy.newProxyInstance(t.getClass().getClassLoader(), 
-				t.getClass().getInterfaces(), new DynProxy());
+				t.getClass().getInterfaces(), new DynProxy(t));
 	}
 }
--- src/main/java/com/learn/java/lang/wangs/pattern/proxy/MainT.java
+++ src/main/java/com/learn/java/lang/wangs/pattern/proxy/MainT.java
@@ -2,17 +2,32 @@ package com.learn.java.lang.wangs.pattern.proxy;
 
 import java.lang.reflect.Proxy;
 
-import javax.crypto.spec.IvParameterSpec;
-
 public class MainT {
 	
 	public static void main(String[] args) {
 		// 动态代理
-//		Test t = new TestImpl();
-//		MyInvo in = new MyInvo(t);
-//		Test p =  (Test)Proxy.newProxyInstance(t.getClass().getClassLoader(), t.getClass().getInterfaces(), in);
+//		Test testImpl = new TestImpl();
+//		MyInvo invocationHandlerMyImpl = new MyInvo(testImpl);
+//		Test p =  (Test)Proxy.newProxyInstance(testImpl.getClass().getClassLoader(), testImpl.getClass().getInterfaces(), invocationHandlerMyImpl);
 //		p.hello();
+		/**
+		 * 主要原理是：反射
+		 * Proxy.newProxyInstance 是java动态代理类要调用的静态方法
+		 * 参数1，被代理的接口的具体实现类的类加载器
+		 * 参数2，被代理的接口的具体实现类集合
+		 * 参数3，接口InvocationHandler的具体实现者，也就是代理类中需要开发人员自定实现的代理部分
+		 */
+//		Test pwrong =  (Test)Proxy.newProxyInstance(Test.class.getClassLoader(),Test.class.getInterfaces(), new MyInvo(new TestImpl()));
+//		Test p1 =  (Test)Proxy.newProxyInstance(TestImpl.class.getClassLoader(),TestImpl.class.getInterfaces(), new MyInvo(new TestImpl()));
+//		p1.hello();
 		// 代理
-		DynProxy.newMapperProxy(Test.class).hello();
+//		DynProxy.newMapperProxy(TestImpl.class).hello();
+		newMapperProxy(new TestImpl().getClass()).hello();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T newMapperProxy(Class<T> t) {
+		return (T)Proxy.newProxyInstance(t.getClass().getClassLoader(), 
+				t.getClass().getInterfaces(), new DynProxy(t));
 	}
 }
