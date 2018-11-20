9edf966d5b3f1683b1229129f5361746c930ecef
wangs <1031897661@qq.com>
2018-11-20 09:58:28 +0800

    add Qeq

--- src/main/java/com/learn/java/lang/wangs/base/string/StringTest.java
+++ src/main/java/com/learn/java/lang/wangs/base/string/StringTest.java
@@ -6,38 +6,51 @@ import java.util.Map.Entry;
 
 public class StringTest {
 
-	public static void main(String[] args) {
-		// Bearer 4df29fbd-b752-4f72-864c-d909ff311400
-//		String s = "Bearer 4df29fbd-b752-4f72-864c-d909ff311400";
-//		
-//		System.out.println(s.substring(7));
-		String s = "E";
-		if (s.equals("A")) {
-			System.out.println("A");
-		} else if(s.equals("B")){
-			System.out.println("B");
-		} else if(s.equals("C")){
-			System.out.println("C");
-		} else {
-			System.out.println("D");
-		}
-        Map<String,String> params = new HashMap<>();
-        params.put("code", "123456");
-        params.put("name","wangs");
-        params.put("sex","man");
-        StringBuffer pramaToStr = new StringBuffer();
-        int size = params.size();
-        for (Entry<String, String> param : params.entrySet()) {
-        	if(size <= 1){
-        		pramaToStr.append(param.getKey()).append("\":\"").append(param.getValue());
-        	}else{
-        		pramaToStr.append(param.getKey()).append("\":\"").append(param.getValue()).append("\",\"");
-        	}
-        	size--;
-        }
-        StringBuffer sb = new StringBuffer();
-        sb.append("{\"").append(pramaToStr.toString()).append("\"}");
-        // {"code":"123456"}
-        System.out.println(sb.toString());
+//	public static void main(String[] args) {
+//		// Bearer 4df29fbd-b752-4f72-864c-d909ff311400
+////		String s = "Bearer 4df29fbd-b752-4f72-864c-d909ff311400";
+////		
+////		System.out.println(s.substring(7));
+//		String s = "E";
+//		if (s.equals("A")) {
+//			System.out.println("A");
+//		} else if(s.equals("B")){
+//			System.out.println("B");
+//		} else if(s.equals("C")){
+//			System.out.println("C");
+//		} else {
+//			System.out.println("D");
+//		}
+//        Map<String,String> params = new HashMap<>();
+//        params.put("code", "123456");
+//        params.put("name","wangs");
+//        params.put("sex","man");
+//        StringBuffer pramaToStr = new StringBuffer();
+//        int size = params.size();
+//        for (Entry<String, String> param : params.entrySet()) {
+//        	if(size <= 1){
+//        		pramaToStr.append(param.getKey()).append("\":\"").append(param.getValue());
+//        	}else{
+//        		pramaToStr.append(param.getKey()).append("\":\"").append(param.getValue()).append("\",\"");
+//        	}
+//        	size--;
+//        }
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("{\"").append(pramaToStr.toString()).append("\"}");
+//        // {"code":"123456"}
+//        System.out.println(sb.toString());
+//	}
+	
+	static void appendStr(StringBuffer sb) {
+		StringBuffer sc = new StringBuffer();
+		sc = sb;
+		sc.append("world");
 	}
+	
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("hello");
+		appendStr(sb);
+		System.out.println(sb.toString());
+	}
+	
 }
---
+++ src/main/java/com/learn/java/lang/wangs/rebuild/ReplaceMehodwithMethodObject.java
@@ -0,0 +1,65 @@
+package com.learn.java.lang.wangs.rebuild;
+
+public class ReplaceMehodwithMethodObject {
+
+	
+	
+/*	@SuppressWarnings("unused")
+	double price(		
+			double a,
+			double b,
+			double c,
+			double d,
+			double e,
+			double f) {
+		a = 1;
+		b = 2;
+		c = 3;
+		d = 4;
+		e = 5;
+		f = 6;
+		// ...
+		return 0;
+	}*/
+	double price(		
+			double a,
+			double b,
+			double c,
+			double d,
+			double e,
+			double f) {
+		return new CountPrice(this, a, b, c, d, e, f).countPrice();
+	}
+	
+	@SuppressWarnings("unused")
+	private class CountPrice{
+		final ReplaceMehodwithMethodObject replaceMehodwithMethodObject;
+		double a;
+		double b;
+		double c;
+		double d;
+		double e;
+		double f;
+		public CountPrice(ReplaceMehodwithMethodObject replaceMehodwithMethodObject, double a, double b, double c, double d, double e, double f) {
+			super();
+			this.replaceMehodwithMethodObject = replaceMehodwithMethodObject;
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+		}
+		// TODO:继续拆分到很多函数
+		double countPrice() {
+			a = 1;
+			b = 2;
+			c = 3;
+			d = 4;
+			e = 5;
+			f = 6;
+			return a;
+		}
+		
+	}
+}
