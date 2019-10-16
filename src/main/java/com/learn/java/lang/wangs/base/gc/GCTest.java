4ab26523422d1b987643020a1e19e5b3aebf4b99
wangs <1031897661@qq.com>
2019-10-16 19:42:57 +0800

    test jvm

--- src/main/java/com/learn/java/lang/wangs/base/gc/GCTest.java
+++ src/main/java/com/learn/java/lang/wangs/base/gc/GCTest.java
@@ -3,6 +3,13 @@ package com.learn.java.lang.wangs.base.gc;
 public class GCTest {
 
     
+//    -Xms20m
+//    -Xmx26m
+//    -XX:+PrintGCDetails
+//    -verbose:gc
+//    -Xloggc:d:/gc.log
+//    -Xmn10M
+    
     public static void main(String[] args) {
         
         for (int i = 0; i < 30; i++) {
